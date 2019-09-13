49fd49d6c629d670636f39beb4f731fa1569803b
ClaudiuBu <37150852+ClaudiuBu@users.noreply.github.com>
2019-09-13 15:47:57 +0300

    Added 4 additional operators

--- js/calculator.js
+++ js/calculator.js
@@ -3,100 +3,112 @@
 var botoaneNumere = document.querySelectorAll(".number");
 var butoaneOperatori = document.querySelectorAll(".operator");
 var rezultat = document.getElementById("rezultat");
+var operatieActuala = document.getElementById("operatie");
+var egal = document.querySelector(".egal-operator");
 
-var addagain = false;
-var displaySpace = 630, firstNumber,secondNumber, oldNumber = "", newNumber = "",operator="",alesOperator=false;
+var displaySpace = 680,
+    firstNumber,
+    secondNumber,
+    oldNumber = "",
+    newNumber = "",
+    operator,
+    operatorGlobal,
+    total,
+    alesOperator = false;
 
 for (var i = 0; i < botoaneNumere.length; i++) {
 
     botoaneNumere[i].addEventListener("click", function () {
-        if(alesOperator==false){
-        oldNumber = this.innerHTML;
-        newNumber = newNumber + oldNumber;
-        firstNumber=newNumber;
-        rezultat.innerHTML = firstNumber;
-        console.log("First Number "+firstNumber);
-        rezultat.style.marginLeft = displaySpace.toString() + "px";
+        if (alesOperator == false) {
+            oldNumber = this.innerHTML;
+            newNumber = newNumber + oldNumber;
+            firstNumber = newNumber;
+            rezultat.innerHTML = firstNumber;
+            console.log("First Number " + firstNumber);
+            rezultat.style.marginLeft = displaySpace.toString() + "px";
         }
 
-        
-        
 
-        if(alesOperator==true){
-        oldNumber = this.innerHTML;
-        newNumber = newNumber + oldNumber;
-        secondNumber=newNumber;
-        rezultat.innerHTML = secondNumber;
-        console.log("Second Number"+secondNumber);
-        rezultat.style.marginLeft = displaySpace.toString() + "px";
+
+
+        if (alesOperator == true) {
+            oldNumber = this.innerHTML;
+            newNumber = newNumber + oldNumber;
+            secondNumber = newNumber;
+            rezultat.innerHTML = secondNumber;
+            console.log("Second Number" + secondNumber);
+            rezultat.style.marginLeft = displaySpace.toString() + "px";
         }
         displaySpace -= 17;
     })
 }  //Add event listeners to numbers
 
 
-for(i = 0; i<butoaneOperatori.length;i++){
-    butoaneOperatori[i].addEventListener("click", function(){
-        
-        var operator = this.innerHTML;
-        oldNumber="";
-        newNumber="";
-        console.log("Operator "+operator);
-        alesOperator=true;
-        displaySpace=630;
-        //compute(firstNumber,secondNumber,operator);
+for (i = 0; i < butoaneOperatori.length; i++) {
+    butoaneOperatori[i].addEventListener("click", function () {
 
-        switch(operator){
-            case 'AC':
-                    rezultat.style.marginLeft = displaySpace.toString() + "px";
-                    oldNumber="";
-                    newNumber="";
-                    rezultat.innerHTML ="0";
-                    alesOperator=false;
-                    break;
-            case '=':
-                //compute(firstNumber,secondNumber,operator);
+        var operator = this.innerHTML;
+        operatorGlobal=operator;
+        rezultat.innerHTML=rezultat.innerHTML+this.innerHTML;
+        oldNumber = "";
+        newNumber = "";
+        console.log("Operator " + operator);
+        alesOperator = true;
+        displaySpace = 680;
+
+        switch (operator) {
+            case 'C':
+                rezultat.style.marginLeft = displaySpace.toString() + "px";
+                oldNumber = "";
+                newNumber = "";
+                rezultat.innerHTML = "0";
+                alesOperator = false;
+                break;
         }
 
     })
 }// add event listener to Operators
 
 
+    egal.addEventListener("click",function(){
+        console.log(operatorGlobal);
+        compute(firstNumber,secondNumber,operatorGlobal);
+        showResult(total);
+    })
+
+
 function depanareLog(arg1) {
     console.log(arg1);
 }
 
-function compute(arg1,arg2,operator){
-    switch(operator){
-        case '+':
-                
-                rezultat.innerHTML=arg1+arg2;
-                console.log("ajunge la compute de adunare");
-        break;
+function compute(arg1, arg2, arg3) {
+    
+    console.log("Ajunge in functia de compute");
+    console.log("Primul numar"+arg1 +"Al doilea numar "+arg2+ " Iar operantul este"+ arg3);
+    switch (arg3) {
+        case  '+':
+            total = parseInt(arg1,10)+parseInt(arg2,10);
+            console.log("ajunge la compute de adunare si totatlul este "+total);
+            break;
+        case '−':
+            total=parseInt(arg1,10)-parseInt(arg2,10);
+            console.log("ajunge la compute de scadere si totatlul este "+total);
+            break;
+        case '×':
+            total=parseInt(arg1,10)*parseInt(arg2,10);
+            console.log("ajunge la compute de inmultire si totatlul este "+total);
+            break;
+        case '÷':
+            total=parseInt(arg1,10)/parseInt(arg2,10);
+            console.log("ajunge la compute de impartire si totatlul este "+total);
+            break;
         default:
-                rezultat.innerHTML =newNumber+oldNumber;
+            rezultat.innerHTML = parseInt(arg1,10)+parseInt(arg2,10);
     }
 
 }
-
-
-
-// function compute(arg1,arg2,operator) {
-
-//     function suma(arg1, arg2) {
-//         return arg1 + arg2;
-//     }
-//     function scadere(arg1, arg2) {
-//         return arg1 - arg2;
-//     }
-//     function inmultire(arg1, arg2) {
-//         return arg1 * arg2;
-//     }
-//     function impartire(arg1, arg2) {
-//         return arg1 / arg2;
-//     }
-
-//     function egal(arg1, arg2, operant) {
-//         return operant(arg1, arg2);
-//     }
-// }
+function showResult(arg1){
+    console.log("Ajung in functie de afisare rezultat!");
+    console.log(arg1);
+    rezultat.innerHTML= arg1.toString();
+}
