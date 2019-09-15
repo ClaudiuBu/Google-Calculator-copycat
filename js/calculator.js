5dfb91c3c996b1e9eddb32dd145f7aeacd06144f
ClaudiuBu <37150852+ClaudiuBu@users.noreply.github.com>
2019-09-15 18:29:19 +0300

    Implemented last calculatios in index.js. Each calculation will be passed in a table with all the operations performed by the user Added footer. Updated CSS

--- js/calculator.js
+++ js/calculator.js
@@ -5,16 +5,23 @@ var butoaneOperatori = document.querySelectorAll(".operator");
 var rezultat = document.getElementById("rezultat");
 var operatieActuala = document.getElementById("operatie");
 var egal = document.querySelector(".egal-operator");
+var historyOperatorBtn = document.querySelector(".fa-history");
 
-var 
+
+
+
+var
     firstNumber,
     secondNumber,
     oldNumber = "",
     newNumber = "",
     operator,
     operatorGlobal,
-    total=1,
-    alesOperator = false;
+    total = 1,
+    alesOperator = false,
+    on = 1
+    rowNum=0,
+    cellNum=0;
 
 for (var i = 0; i < botoaneNumere.length; i++) {
 
@@ -25,7 +32,7 @@ for (var i = 0; i < botoaneNumere.length; i++) {
             firstNumber = newNumber;
             rezultat.innerHTML = firstNumber;
             console.log("First Number " + firstNumber);
-            
+
         }
 
 
@@ -35,10 +42,10 @@ for (var i = 0; i < botoaneNumere.length; i++) {
             oldNumber = this.innerHTML;
             newNumber = newNumber + oldNumber;
             secondNumber = newNumber;
-            rezultat.innerHTML =rezultat.innerHTML+this.innerHTML;
+            rezultat.innerHTML = rezultat.innerHTML + this.innerHTML;
             console.log("Second Number" + secondNumber);
         }
-        
+
     })
 }  //Add event listeners to numbers
 
@@ -46,21 +53,21 @@ for (var i = 0; i < botoaneNumere.length; i++) {
 for (i = 0; i < butoaneOperatori.length; i++) {
     butoaneOperatori[i].addEventListener("click", function () {
 
-        
+
         var operator = this.innerHTML;
-        operatorGlobal=operator;
-        rezultat.innerHTML=rezultat.innerHTML+operator;
+        operatorGlobal = operator;
+        rezultat.innerHTML = rezultat.innerHTML + operator;
         oldNumber = "";
         newNumber = "";
         console.log("Operator " + operator);
         alesOperator = true;
-       
+
 
         switch (operator) {
             case 'C':
                 oldNumber = "";
                 newNumber = "";
-                total=1;
+                total = 1;
                 rezultat.innerHTML = "0";
                 alesOperator = false;
                 break;
@@ -69,7 +76,7 @@ for (i = 0; i < butoaneOperatori.length; i++) {
                 showResult(total);
                 break;
             case 'x!':
-                total = permutari(firstNumber,total);
+                total = permutari(firstNumber, total);
                 showResult(total);
                 break;
             case 'ln':
@@ -84,15 +91,79 @@ for (i = 0; i < butoaneOperatori.length; i++) {
 }// add event listener to Operators
 
 
-    egal.addEventListener("click",function(){
-        
-        console.log(operatorGlobal);
-        compute(firstNumber,secondNumber,operatorGlobal);
-        moveOperations();
-        showResult(total);
-        displaySpace = displaySpace -(17 *total.lenth);
-        logicFlows();
-    })
+egal.addEventListener("click", function () {
+
+    console.log(operatorGlobal);
+    compute(firstNumber, secondNumber, operatorGlobal);
+    moveOperations();
+    showResult(total);
+    addNode();
+    logicFlows();
+})
+
+
+historyOperatorBtn.addEventListener("click", function () {
+    toggleHistoryContainer();
+    
+
+});
+
+function toggleHistoryContainer(){
+    switch (on) {
+        case 0:
+            document.querySelector(".container-history").classList.remove("visible");
+            historyOperatorBtn.classList.remove("blue");
+            on = 1;
+            break;
+        case 1:
+            document.querySelector(".container-history").classList.add("visible");
+            historyOperatorBtn.classList.add("blue");
+            on = 0;
+            break;
+    }
+}
+
+function addNode(){
+var result,body;
+    result=total;
+    body=firstNumber+operatorGlobal+secondNumber;
+    equal=" = ";
+
+    rowNum++;cellNum++;
+
+    var rowClass="row_"+rowNum.toString();
+    var cellClass="cell_"+cellNum.toString();
+    cellNum++;
+    var cellClass2="cell_"+cellNum.toString();
+    cellNum++;
+    var cellClass3="cell_"+cellNum.toString();
+
+var node = document.createElement("tr");
+node.className =rowClass;
+
+var childNode = document.createElement("td");
+var childNode2 = document.createElement("td");
+var childNode3 = document.createElement("p");
+
+childNode.className =  cellClass;
+childNode2.className= cellClass3;
+childNode3.className= cellClass2;
+
+
+//var textnode = document.createTextNode(firstNumber+" "+operatorGlobal+" "+secondNumber+" = "+total);
+var textnodeLeft = document.createTextNode(body);
+var textnodeRight = document.createTextNode(result);
+var textnodeBetween = document.createTextNode(equal);
+
+
+document.querySelector("table").appendChild(node);
+document.querySelector("."+rowClass).appendChild(childNode);
+document.querySelector("."+rowClass).appendChild(childNode3);
+document.querySelector("."+rowClass).appendChild(childNode2);
+childNode.appendChild(textnodeLeft);
+childNode2.appendChild(textnodeRight);
+childNode3.appendChild(textnodeBetween);
+}
 
 //Functii;
 function depanareLog(arg1) {
@@ -100,60 +171,60 @@ function depanareLog(arg1) {
 }
 
 function compute(arg1, arg2, arg3) {
-    
+
     console.log("Ajunge in functia de compute");
-    console.log("Primul numar"+arg1 +"Al doilea numar "+arg2+ " Iar operantul este"+ arg3);
+    console.log("Primul numar" + arg1 + "Al doilea numar " + arg2 + " Iar operantul este" + arg3);
     switch (arg3) {
-        case  '+':
-            total = parseInt(arg1,10)+parseInt(arg2,10);
-            console.log("ajunge la compute de adunare si totatlul este "+total);
+        case '+':
+            total = parseInt(arg1, 10) + parseInt(arg2, 10);
+            console.log("ajunge la compute de adunare si totatlul este " + total);
             break;
         case '−':
-            total=parseInt(arg1,10)-parseInt(arg2,10);
-            console.log("ajunge la compute de scadere si totatlul este "+total);
+            total = parseInt(arg1, 10) - parseInt(arg2, 10);
+            console.log("ajunge la compute de scadere si totatlul este " + total);
             break;
         case '×':
-            total=parseInt(arg1,10)*parseInt(arg2,10);
-            console.log("ajunge la compute de inmultire si totatlul este "+total);
+            total = parseInt(arg1, 10) * parseInt(arg2, 10);
+            console.log("ajunge la compute de inmultire si totatlul este " + total);
             break;
         case '÷':
-            total=parseInt(arg1,10)/parseInt(arg2,10);
-            console.log("ajunge la compute de impartire si totatlul este "+total);
+            total = parseInt(arg1, 10) / parseInt(arg2, 10);
+            console.log("ajunge la compute de impartire si totatlul este " + total);
             break;
         case 'x<sup>y</sup>':
-            total=Math.pow(arg1,arg2);
-            console.log("Ajunge la compute exponential si totalul este "+total);
+            total = Math.pow(arg1, arg2);
+            console.log("Ajunge la compute exponential si totalul este " + total);
             break;
         case '%':
-            total = (arg1 *arg2)/100;
+            total = (arg1 * arg2) / 100;
             console.log("Ajunge in functia procent");
             break;
-        
+
 
         default:
-            rezultat.innerHTML = parseInt(arg1,10)+parseInt(arg2,10);
+            rezultat.innerHTML = parseInt(arg1, 10) + parseInt(arg2, 10);
     }
 
 }
-function permutari(arg1,result){
-    while(arg1>=2){
+function permutari(arg1, result) {
+    while (arg1 >= 2) {
 
-        result *= arg1*(arg1-1);
-        arg1=arg1-2;
+        result *= arg1 * (arg1 - 1);
+        arg1 = arg1 - 2;
     }
     return result;
 }
 
-function showResult(arg1){
+function showResult(arg1) {
     console.log("Ajung in functie de afisare rezultat!");
     console.log(arg1);
-    rezultat.innerHTML= arg1.toString();
+    rezultat.innerHTML = arg1.toString();
 }
-function logicFlows(){
-    if(rezultat.innerHTML =="NaN"){
+function logicFlows() {
+    if (rezultat.innerHTML == "NaN") {
         document.querySelector(".erase").click();
     }
 }
-function moveOperations(){
-    document.querySelector(".prior-operation").innerHTML=rezultat.innerHTML+" = ";
+function moveOperations() {
+    document.querySelector(".prior-operation").innerHTML = rezultat.innerHTML + " = ";
 }
